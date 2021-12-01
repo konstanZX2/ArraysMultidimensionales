ffc1cc5e097006e66960c8651e56f5eaf4a3b8d8
konstanZX2 <zxgenerationzx@gmail.com>
2021-12-01 19:30:54 +0100

    eeeeeeeeeeeeeeeeeeeeee

--- src/com/company/Ejercicio1.java
+++ src/com/company/Ejercicio1.java
@@ -11,7 +11,7 @@ public class Ejercicio1 {
         int filas = escaneado.nextInt();
         System.out.println("Introduzca el numero de columnas array");
         int columnas = escaneado.nextInt();
-        int [][] array = new int[filas][columnas];
+        int[][] array = new int[filas][columnas];
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
                 System.out.print("A[" + i + "][" + j + "]= ");
@@ -30,17 +30,18 @@ public class Ejercicio1 {
         System.out.println();
 
 
-
         //Editor de un elemento del array
         System.out.println("Ahora que esta creado el Array elige la posicion que deseas cambiar");
         System.out.println();
         System.out.println("Selecciona el numero de fila");
-        int filaUsuario=escaneado.nextInt();
+        int filaUsuario = escaneado.nextInt();
         System.out.println("Selecciona el numero de columna");
-        int columnaUsuario= escaneado.nextInt();
+        int columnaUsuario = escaneado.nextInt();
         System.out.println("Selecciona el valor que vas a colocar en esa posicion");
-        int numeroUsuario=escaneado.nextInt();
-        array[filaUsuario][columnaUsuario]= numeroUsuario;
+        int numeroUsuario = escaneado.nextInt();
+        array[filaUsuario][columnaUsuario] = numeroUsuario; int copiaNumeroUsuario=numeroUsuario;
+
+
 
 
         //Lector del array con el elemento cambiado
